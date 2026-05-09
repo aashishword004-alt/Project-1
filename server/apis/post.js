ab21c05958aa64b95f47733321977eca3c43ea69
aashishword004-alt <aashishword004@gmail.com>
2026-05-09 22:45:59 +0530

    upload file or photo done

--- server/apis/post.js
+++ server/apis/post.js
@@ -1,1 +1,26 @@
-// company post apis 
+let express = require('express');
+let app = express();
+
+app.use(express.json());    
+app.use(express.urlencoded({ extended: true }));
+const cors = require('cors');
+
+let upload = require('../apis/multer');
+
+app.use(cors(
+    origin = "http://localhost:5000",
+    methods = ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials = true  
+));
+
+app.post('/upload', upload.single('file'), (req, res) => {
+//    res.send("File Uploaded Successfully");
+console.log(req.body , req.file);
+});
+
+
+
+let port = 5000;
+app.listen(port , () =>{
+     console.log(`Server is running on port ${port}`);
+})
