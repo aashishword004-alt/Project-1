b8d928e66c6137b1e0c555561be938a9089201f3
aashishword004-alt <aashishword004@gmail.com>
2026-08-14 22:45:18 +0530

    for education

---
+++ server/apis/education.js
@@ -0,0 +1,60 @@
+let express = require('express');
+let app = express();
+
+// middleware;
+
+let bodyparser = require('body-parser');
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extends: true }));
+
+// database 
+let connect = require('../database/connection')
+
+// routes
+let EDUC = '/education';
+
+app.get(EDUC, (req, res) => {
+    res.json('Welcome to Education API');
+});
+
+app.put(EDUC + '/upload', (req, res) => {
+
+    //person_id , name_uni, degree_name ,course_name , passing_year , grade , percentage
+    let { person_id, name_uni, degree_name, course_name, passing_year, grade, percentage } = req.body;
+
+    if (!person_id || !name_uni || !degree_name || !course_name || !passing_year || !grade || !percentage) {
+        return res.status(400).json({ error: 'All fields are required' });
+    }
+    else {
+        let sql = 'insert into education (person_id , name_uni, degree_name ,course_name , passing_year , grade , percentage) values (?,?,?,?,?,?,?)';
+        let values = [person_id, name_uni, degree_name, course_name, passing_year, grade, percentage];
+        connect.con.query(sql, values, (err, result) => {
+            if (err) {
+                res.json([{ 'error': true },
+                {
+                    'success': false
+                },
+                {
+                    'message': 'somthing wrong in server'
+                }]);
+            }
+            else {
+                res.json([{ 'error': false },
+                {
+                    'success': true
+                },
+                {
+                    'message': 'Education Details Uploaded Successfully'
+                }
+                ]);
+            }
+        })
+    }
+
+});
+
+
+let port = 3000;
+app.listen(port, () => {
+    console.log('Server is Running On port ' + port);
+})  
--- server/apis/users.js
+++ server/apis/users.js
@@ -446,6 +446,7 @@ app.put(ADMIN + '/forgotpassword', (req, res) => {
 
 
 
+
 let port = 3000;
 app.listen(port, () => {
     console.log('Server is running on port ' + port);
