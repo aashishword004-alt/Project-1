a2976af9de634ef772daa0f3d76855fd2d4b70ea
aashishword004-alt <aashishword004@gmail.com>
2026-08-28 16:00:41 +0530

    handle rest issue

--- server/apis/chat.js
+++ server/apis/chat.js
@@ -4,9 +4,13 @@ let connect = require('../database/connection')
 
 let bodyarser = require('body-parser')
 app.use(bodyarser.json())
-app.use(bodyarser.urlencoded({extended : true}))
+app.use(bodyarser.urlencoded({ extended: true }))
+
 
 const CHAT = '/message'
+
+
+
 
 app.get(CHAT, (req, res) => {
 
@@ -21,8 +25,52 @@ app.get(CHAT, (req, res) => {
     })
 })
 
+
+app.post(CHAT + '/conversation', (req, res) => {
+    let { sender, receiver } = req.body
+    console.log(sender , receiver)
+    if (!sender || !receiver) {
+        res.json([{ 'error': true },
+        {
+            'success': false
+        },
+        {
+            'message': 'input is missing'
+        }
+        ])
+    }
+    else {
+        let sql = `select cp1.conversation_id FROM conversation_participants cp1 JOIN
+                    conversation_participants cp2 on cp1.conversation_id = cp2.conversation_id
+                    JOIN conversations c on c.id = cp1.conversation_id WHERE cp1.user_id = ? and cp2.user_id = ? and c.type = 'direct' `
+
+        let value = [sender, receiver]
+        connect.con.query(sql, value, (error, result) => {
+            if (error) {
+                res.json([{
+                    'error': true
+                },
+                {
+                    'success': false
+                },
+                {
+                    'message': 'somthing wrong in server '
+                }])
+            }
+            else{
+                if(result.length  === 0)
+                {
+                    res.json([{'error' : 'no row found'}])
+                }
+            }
+        })
+    }
+})
+
+
 app.post(CHAT, (req, res) => {
     let { conversation_id, sender_id, content } = req.body
+    console.log(conversation_id,sender_id,content)
     if (!conversation_id || !sender_id || !content) {
         res.json([{ 'error': true },
         {
@@ -51,7 +99,7 @@ app.post(CHAT, (req, res) => {
                     'message': 'message sent successfully'
                 },
                 {
-                  'result' : result.insertID
+                    'result': result.insertID
                 }])
             }
 
